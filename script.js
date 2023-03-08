1e169a648ba4d7576e758ae9fa03cb4215d04400
AjoyKumarPujari <ajoykumarpujari@gmail.com>
2023-03-08 15:51:27 -0400

    Making Js Funuction and HTML UI for password generate project

---
+++ script.js
@@ -0,0 +1,109 @@
+const inputSlider = document.querySelector("[data-lengthSlider]");
+const lengthDisplay = document.querySelector("[data-lengthNumber]");
+const passwordDisplay = document.querySelector("[data-passwordDisplay]");
+const copyBtn = document.querySelector("[data-copy]");
+const copyMsg = document.querySelector("[data-copyMsg]");
+const uppercaseCheck = document.querySelector("#uppercase");
+const lowercaseCheck = document.querySelector("#lowercase");
+const numbersCheck = document.querySelector("#numbers");
+const symbolsCheck = document.querySelector("#symbols");
+const indicator = document.querySelector("[data-indicator]");
+const generateBtn = document.querySelector(".generateButton");
+const allCheckBox = document.querySelectorAll("input[type=checkbox]");
+const symbols = '~`!@#$%^&*()_-+={[}]|:;"<,>.?/';
+
+let password = "";
+let passwordLength = 10;
+let checkCount = 1;
+handleSlider() ;
+//set strength circle color grey
+
+//set password length
+function handleSlider() {
+    inputSlider.value = passwordLength;
+    lengthDisplay.innerText = passwordLength;
+}
+
+function setIndicator(color){
+    indicator.style.background = color;   
+}
+
+//getRandomInteger
+function getRandomInteger(min, max){
+    Math.floor(Math.random() *(max-min)) + min;
+}
+
+function generateRandomNumber(){
+    return getRandomInteger(0, 9);
+}
+
+function generateRandomLowerCase(){
+    return String.fromCharCode.getRandomInteger(97, 123);
+}
+
+function generateRandomUpperCase(){
+    return String.fromCharCode.getRandomInteger(65, 91);
+}
+
+function generateSymbol(){
+    const randNum = getRandomInteger(0, symbols.length);
+    return symbols.charAt(randNum);
+}
+
+function calcStrength(){
+    let hasUpper = false;
+    let hasLower = false;
+    let hasNum = false;
+    let hasSym = false;
+    if (uppercaseCheck.checked) hasUpper = true;
+    if (lowercaseCheck.checked) hasLower = true;
+    if (numbersCheck.checked) hasNum = true;
+    if (symbolsCheck.checked) hasSym = true;
+  
+    if (hasUpper && hasLower && (hasNum || hasSym) && passwordLength >= 8) {
+      setIndicator("#0f0");
+    } else if (
+      (hasLower || hasUpper) &&
+      (hasNum || hasSym) &&
+      passwordLength >= 6
+    ) {
+      setIndicator("#ff0");
+    } else {
+      setIndicator("#f00");
+    }
+}
+
+//copy to clickboard
+async function copyContent() {
+    try {
+        await navigator.clipboard.writeText(passwordDisplay.value);
+        copyMsg.innerText = "copied";
+    }
+    catch(e) {
+        copyMsg.innerText = "Failed";
+    }
+    //to make copy wala span visible
+    copyMsg.classList.add("active");
+
+    setTimeout( () => {
+        copyMsg.classList.remove("active");
+    },2000);
+
+}
+
+
+inputSlider.addEventListener('input', (e)=>{
+    passwordLength = e.target.value;
+    handleSlider();
+})
+
+copyBtn.addEventListener('click', () => {
+    if(passwordDisplay.value){
+        copyContent();
+    }
+})
+// generate Password
+generateBtn.addEventListener('click', () => {
+    
+})
+ 
