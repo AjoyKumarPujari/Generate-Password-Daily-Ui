208e95c8bc13b48427b7374786394da7c7b1b3cd
AjoyKumarPujari <ajoykumarpujari@gmail.com>
2023-03-10 11:43:01 -0400

    Password Generrator FrontEnd and JS add

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const symbols = '~`!@#$%^&*()_-+={[}]|:;"<,>.?/';
 
 let password = "";
 let passwordLength = 10;
-let checkCount = 1;
+let checkCount = 0;
 handleSlider() ;
 //set strength circle color grey
 
@@ -30,7 +30,7 @@ function setIndicator(color){
 
 //getRandomInteger
 function getRandomInteger(min, max){
-    Math.floor(Math.random() *(max-min)) + min;
+    return Math.floor(Math.random() *(max-min)) + min;
 }
 
 function generateRandomNumber(){
@@ -38,11 +38,11 @@ function generateRandomNumber(){
 }
 
 function generateRandomLowerCase(){
-    return String.fromCharCode.getRandomInteger(97, 123);
+    return String.fromCharCode(getRandomInteger(97, 123));
 }
 
 function generateRandomUpperCase(){
-    return String.fromCharCode.getRandomInteger(65, 91);
+    return String.fromCharCode(getRandomInteger(65, 91));
 }
 
 function generateSymbol(){
@@ -91,6 +91,37 @@ async function copyContent() {
 
 }
 
+function suffflePassword(array){
+    //fisher rates method
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+      }
+    let str = "";
+    array.forEach((el) => (str += el));
+    return str;
+}
+
+function handleCheckboxChange() {
+    checkCount = 0;
+    allCheckBox.forEach((checkbox)=>{
+        if(checkbox.checked)
+            checkCount++;
+    });
+
+    //special condition
+    if(passwordLength < checkCount ){
+        passwordLength = checkCount; 
+        handleSlider();
+    }
+}
+
+allCheckBox.forEach( (checkbox)=>{
+    checkbox.addEventListener('change', handleCheckboxChange);
+})
+
 
 inputSlider.addEventListener('input', (e)=>{
     passwordLength = e.target.value;
@@ -104,6 +135,70 @@ copyBtn.addEventListener('click', () => {
 })
 // generate Password
 generateBtn.addEventListener('click', () => {
+    //none of the checkbox are selected
+
+    if(checkCount == 0) 
+    return;
+
+    if(passwordLength < checkCount){
+        passwordLength = checkCount;
+        handleSlider();
+    }
+    //lets start to generate new pasasword
+
+    console.log("starting the journey");
+    //remove all password
+    password = "";
+
+    //lets  put the staff mention by checkboxes
+
+    // if(uppercaseCheck.checked){
+    //     password += generateRandomUpperCase();
+    // }
+    // if(lowercaseCheck.checked){
+    //     password += generateRandomLowerCase();
+    // }
+    // if(numbersCheck.checked){
+    //     password += generateRandomNumber();
+    // }
+    // if(symbolsCheck.checked){
+    //     password += generateSymbol();
+    // }
+
+    let functionArr =[];
+
+    if(uppercaseCheck.checked)
+        functionArr.push(generateRandomUpperCase);
+
+    if(lowercaseCheck.checked)
+        functionArr.push(generateRandomLowerCase);
     
-})
+    if(numbersCheck.checked)
+        functionArr.push(generateRandomNumber);
+    
+    if(symbolsCheck.checked)
+        functionArr.push(generateSymbol);
+
+        //compulsary addition
+        for(let i=0; i<functionArr.length; i++){
+            password+=functionArr[i]();
+        }
+        console.log("compulsary addition done");
+
+        //remaining addition
+        for (let i=0; i<passwordLength-functionArr.length; i++){
+            let randIndex = getRandomInteger(0, functionArr.length)
+            password += functionArr[randIndex]();
+        }
+        console.log("remaining addition done");
+        //shufffled
+        password = suffflePassword(Array.from(password));
+        console.log("sufffling done");
+        //show in Ui
+        passwordDisplay.value = password;
+        console.log("UI addition done");
+        //calculate strength
+        calcStrength();
+        
+});
  
